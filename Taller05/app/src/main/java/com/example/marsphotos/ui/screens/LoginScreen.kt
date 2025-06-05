01e629673a6de094b6dbcad67f71bd2d6a8c8472
Myke Cevallos <mykecevallos@gmail.com>
2025-06-05 18:18:35 -0500

    'Taller05'

---
+++ Taller05/app/src/main/java/com/example/marsphotos/ui/screens/LoginScreen.kt
@@ -0,0 +1,81 @@
+package com.example.marsphotos.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import com.example.marsphotos.R
+
+@Composable
+fun LoginScreen(onLoginSuccess: () -> Unit) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var showError by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        // Imagen del logo (asegúrate de poner el archivo en drawable)
+        Image(
+            painter = painterResource(id = R.drawable.logo), // Usa el nombre de tu imagen
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(360.dp)
+                .padding(bottom = 32.dp)
+        )
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Usuario") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        if (showError) {
+            Text(
+                "Usuario o contraseña incorrectos",
+                color = Color.Red,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(
+            onClick = {
+                if (username == "admin" && password == "1234") {
+                    onLoginSuccess()
+                } else {
+                    showError = true
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Ingresar a la app")
+        }
+    }
+}
